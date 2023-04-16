f86c112b6384dd3fe0a4f723bc05176ee3418391
david-s-kim <mycodingtool9@gmail.com>
2023-04-16 21:26:10 +0900

    단어장 앱

--- app/src/main/java/com/example/chapter6/AddActivity.kt
+++ app/src/main/java/com/example/chapter6/AddActivity.kt
@@ -1,26 +1,43 @@
 package com.example.chapter6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.children
 import com.example.chapter6.databinding.ActivityAddBinding
 import com.google.android.material.chip.Chip
 
 class AddActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddBinding
+    private var originWord: Word? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initViews()
+        binding.addButton.setOnClickListener {
+            if(originWord == null) add() else edit()
+        }
     }
 
-    private fun initViews(){
+    private fun initViews() {
         val types = listOf("명사", "동사", "대명사", "형용사", "부사", "감탄사", "전치사", "접속사")
         binding.typeChipGroup.apply {
-            types.forEach{ text ->
+            types.forEach { text ->
                 addView(createChip(text))
             }
+        }
+
+        originWord = intent.getParcelableExtra("originWord")
+        originWord?.let { word ->
+            binding.textINputEditText.setText(word.text)
+            binding.meanTextInputEditText.setText(word.mean)
+            val selectedChip = binding.typeChipGroup.children.firstOrNull {
+                (it as Chip).text == word.text
+            } as? Chip
+            selectedChip?.isChecked = true
         }
     }
 
@@ -30,5 +47,41 @@ class AddActivity : AppCompatActivity() {
             isCheckable = true
             isClickable = true
         }
+    }
+
+    private fun add() {
+        val text = binding.textINputEditText.text.toString()
+        val mean = binding.meanTextInputEditText.text.toString()
+        val type = findViewById<Chip>(binding.typeChipGroup.checkedChipId).text.toString()
+        val word = Word(text, mean, type)
+
+        Thread {
+            AppDatabase.getInstance(this)?.wordDao()?.insert(word)
+            runOnUiThread {
+                Toast.makeText(this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show()
+            }
+            val intent = Intent().putExtra("isUpdated", true)
+            setResult(RESULT_OK, intent)
+            finish()
+        }.start()
+    }
+
+    private fun edit() {
+        val text = binding.textINputEditText.text.toString()
+        val mean = binding.meanTextInputEditText.text.toString()
+        val type = findViewById<Chip>(binding.typeChipGroup.checkedChipId).text.toString()
+        val editWord = originWord?.copy(text = text, mean = mean, type = type)
+
+        Thread {
+            editWord?.let { word ->
+                AppDatabase.getInstance(this)?.wordDao()?.update(word)
+                val intent = Intent().putExtra("editWord", editWord)
+                setResult(RESULT_OK, intent)
+                runOnUiThread{
+                    Toast.makeText(this, "수정을 완료했습니다.", Toast.LENGTH_SHORT).show()
+                }
+                finish()
+            }
+        }.start()
     }
 }
--- app/src/main/java/com/example/chapter6/MainActivity.kt
+++ app/src/main/java/com/example/chapter6/MainActivity.kt
@@ -4,13 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chapter6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var wordAdapter: WordAdapter
+    private var selectedWord: Word? = null
+
+    private val updateAddWordResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val isUpdated = result.data?.getBooleanExtra("isUpdated", false) ?: false
+        if (result.resultCode == RESULT_OK && isUpdated) {
+            updatedAddWord()
+        }
+    }
+
+    private val updateEditWordResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val editWord = result.data?.getParcelableExtra<Word>("editWord")
+        if (result.resultCode == RESULT_OK && editWord != null) {
+            updateEditWord(editWord)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,32 +39,97 @@ class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
 
         initRecyclerView()
         binding.addButton.setOnClickListener {
+
+
             Intent(this, AddActivity::class.java).let {
-                startActivity(it)
+                updateAddWordResult.launch(it)
             }
+        }
+
+        binding.deleteImageView.setOnClickListener {
+            delete()
+        }
+        binding.editImageView.setOnClickListener {
+            edit()
         }
     }
 
 
     private fun initRecyclerView() {
-        val dummyList = mutableListOf(
-            Word("weather", "날씨", "명사"),
-            Word("honey", "꿀", "명사"),
-            Word("run", "실행하다", "동사"),
-        )
 
-        wordAdapter = WordAdapter(dummyList, this)
+        wordAdapter = WordAdapter(mutableListOf(), this)
         binding.wordRecyclerView.apply {
             adapter = wordAdapter
-            layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            layoutManager =
+                LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
 
             // UI
-            val dividerItemDecoration = DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
+            val dividerItemDecoration =
+                DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
             addItemDecoration(dividerItemDecoration)
+        }
+
+        Thread {
+            // 데이터를 가져온다 -> list에 넣는다.
+            val list = AppDatabase.getInstance(this)?.wordDao()?.getAll() ?: emptyList()
+            wordAdapter.list.addAll(list)
+            // 어댑터에게 데이터를 넣었다는 내용을 추가해줘야한다.
+            runOnUiThread {
+                wordAdapter.notifyDataSetChanged()
+            }
+        }.start()
+    }
+
+    private fun updatedAddWord() {
+        Thread {
+            AppDatabase.getInstance(this)?.wordDao()?.getLatestWord()?.let { word ->
+                wordAdapter.list.add(0, word)
+                // 데이터를 추가하고 어댑터에게 알려주야함
+                runOnUiThread {
+                    wordAdapter.notifyDataSetChanged()
+                }
+            }
+        }.start()
+    }
+
+    private fun updateEditWord(word: Word) {
+        val index = wordAdapter.list.indexOfFirst { it.id == word.id }
+        wordAdapter.list[index] = word
+        runOnUiThread {
+            selectedWord = word
+            wordAdapter.notifyItemChanged(index)
+            binding.textTextView.text = word.text
+            binding.meanTextView.text = word.mean
         }
     }
 
+    private fun delete() {
+        if (selectedWord == null) return
+
+        Thread {
+            selectedWord?.let { word ->
+                AppDatabase.getInstance(this)?.wordDao()?.delete(word)
+                runOnUiThread {
+                    wordAdapter.list.remove(word)
+                    wordAdapter?.notifyDataSetChanged()
+                    binding.textTextView.text = ""
+                    binding.meanTextView.text = ""
+                    Toast.makeText(this, "삭제가 완료됐습니다.", Toast.LENGTH_SHORT).show()
+                }
+                selectedWord = null
+            }
+        }.start()
+    }
+    private fun edit(){
+        if(selectedWord == null) return
+
+        val intent = Intent(this, AddActivity::class.java).putExtra("originWord", selectedWord)
+        updateEditWordResult.launch(intent)
+    }
+
     override fun onClick(word: Word) {
-        Toast.makeText(this, "${word.text} 가 클릭 됐습니다.", Toast.LENGTH_SHORT).show()
+        selectedWord = word
+        binding.textTextView.text = word.text
+        binding.meanTextView.text = word.mean
     }
 }
